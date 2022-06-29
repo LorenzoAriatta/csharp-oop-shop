fdee913b99430a44748349a6ddfd6f1aa388c6a4
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-29 14:34:20 +0200

    Create class Product, implemented logic for setPrice | setCode and setIva

---
+++ Prodotto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class Product
+{
+	public int Code
+	{
+		get
+		{
+			return Code;
+		}
+		set
+		{
+			Code = this.CreateCode();
+		}
+	}
+
+	public uint Price 
+	{ 
+		get
+		{
+			return Price;
+		}
+		set 
+		{
+			Price = this.SetPrice();
+		} 
+	}
+
+	public int Iva 
+	{ 
+		get 
+		{ 
+			return Iva; 
+		} 
+		set 
+		{ 
+			Iva = value; 
+		} 
+	}	
+
+	public string name;
+	public int description;
+	
+	public Product()
+	{
+		
+	}
+
+	public int CreateCode()
+    {
+		Random rnd = new Random();
+		int randomCode = rnd.Next(1, 99999999);
+		return randomCode;
+	}
+
+	public uint SetPrice()
+    {
+		Random random = new Random();
+		uint randomPrice = (uint)random.Next(0, 1000000);
+		return randomPrice;
+    }
+}
